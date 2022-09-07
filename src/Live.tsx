f2fb80d1e4bec838348167204123b0b020e25621
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-09-06 23:16:00 -0500

    feat: added basic routing for the live

---
+++ src/Live.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Live = () => <div />;
+
+export default Live;
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import Live from './Live';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (window.location.pathname === '/live') {
+  ReactDOM.render(<Live />, document.getElementById('root'));
+} else if (window.location.pathname === '/ideas') {
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
